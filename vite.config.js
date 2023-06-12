07010555068f184b3876d6e681c46bc43e41f564
Полина Куксова <pakuksova@gmail.com>
2023-06-13 00:06:39 +0300

    add plugin for copy static folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   build: {
     outDir: resolve(__dirname, 'dist'),
   }, 
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(resolve(__dirname, './static')),
+          dest: './',
+        },
+      ],
+    }),
+  ],
 }) 
