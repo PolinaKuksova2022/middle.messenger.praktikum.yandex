237e6237691db08acd0592328743b0e19a7672c7
Полина Куксова <pakuksova@gmail.com>
2023-06-13 12:07:31 +0300

    return app.get

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
 
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static('./dist'));
 
 
-// app.get('*', (req, res) => {
-// 	res.sendFile('./dist/index.html', { root: __dirname })
-// })
+app.get('*', (req, res) => {
+	res.sendFile('./dist/index.html', { root: __dirname })
+})
 
 app.use((req, res) => {
 	res.status(404).send('./dist');
