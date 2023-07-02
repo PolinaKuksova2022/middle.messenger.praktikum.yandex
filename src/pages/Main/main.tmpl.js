03a92cf5432fa4b3e36b0aa06c2e943ac4b833e6
Полина Куксова <pakuksova@gmail.com>
2023-07-02 17:41:33 +0300

    delete files

--- src/pages/Main/main.tmpl.js
+++
@@ -1,8 +0,0 @@
-export const template = `
-<div class="container">
-  <h1 class="container__title">Навигация</h1>
-    {{#each buttons}}
-        {{{this}}}
-    {{/each}}
-</div>
-`;
--- src/utils/changeData.js
+++
@@ -1,7 +0,0 @@
-// let data = document.getElementsByClassName('group-data__value');
-// let section = document.getElementsByClassName('profile-container__btn');
-
-// export function changeData() {
-//     let editText= `<textarea class="edit">${data.textContent}</textarea>`;
-//     data.innerHTML = editText;
-// }
