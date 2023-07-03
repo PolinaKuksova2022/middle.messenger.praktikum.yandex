2733b8c870427e2dcf5ee3fc3fd35ad3e0fbea6b
Полина Куксова <pakuksova@gmail.com>
2023-07-03 11:54:40 +0300

    add nav for main page

--- src/pages/Main/main.tmpl.ts
+++ src/pages/Main/main.tmpl.ts
@@ -1,11 +1,17 @@
-export const template = `
+const template = `
 <div class="container">
   <h1 class="container__title">Навигация</h1>
-    {{{button_1}}}
-    {{{button_2}}}
-    {{{button_3}}}
-    {{{button_4}}}
-    {{{button_5}}}
-    {{{button_6}}}
+    <nav class="container__nav">
+      <ul>
+        <li>{{{button_1}}}</li>
+        <li>{{{button_2}}}</li>
+        <li>{{{button_3}}}</li>
+        <li>{{{button_4}}}</li>
+        <li>{{{button_5}}}</li>
+        <li>{{{button_6}}}</li>
+      </ul>
+    </nav>
 </div>
 `;
+
+export default template;
